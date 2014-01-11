49e068d0d3f287c2ecf05f5c45c501737ce03b3a
Takuma Nakajima <penguin.trance.2716@gmail.com>
2014-01-12 04:44:27 +0900

    fix checkbox to change show/hidden .files

--- app/assets/javascripts/webfiler.js
+++ app/assets/javascripts/webfiler.js
@@ -1,13 +1,35 @@
 function reload(path) {
-    var checkbox = document.getElementById('showall');
-    checkbox.checked = !(checkbox.checked);
+    var checkbox = document.getElementById('checkboximg');
     var currentPath = document.getElementById('currentpath').innerText;
     var prefix;
-    if (checkbox.checked == true) {
-	prefix = "/listall"
-    } else {
+    if (checkbox.getAttribute("checked") == "true") {
 	prefix = "/list"
+    } else {
+	prefix = "/listall"
     }
     var html = "<iframe id=\"filelist\" src=\"" + prefix + currentPath + "\" width=\"100%\" height=\"98%\" frameborder=\"0\" scrolling=\"auto\"></iframe>";
+
     document.getElementById('filelist_frame').innerHTML = html;
+
+
+
+    html = "<span onClick=\"reload('" + path + "')\">"
+    html += "<table border=\"0\">"
+    html += "<tr valign=\"middle\">"
+    html += "<td>"
+    if (checkbox.getAttribute("checked") == "true") {
+	html += "<img id=\"checkboximg\" checked=\"false\" src=\"/icon/checkbox-empty.png\" height=\"20\" style=\"vertical-align:middle;\" />"
+    } else {
+	html += "<img id=\"checkboximg\" checked=\"true\" src=\"/icon/checkbox.png\" height=\"20\" style=\"vertical-align:middle;\" />"
+    }
+    html += "</td>"
+    html += "<td>"
+    html += "Show hidden files"
+    html += "</td>"
+    html += "</tr>"
+    html += "</table>"
+    html += "</span>"
+
+
+    document.getElementById('checkbox').innerHTML = html;
 }
